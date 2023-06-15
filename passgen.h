fefe25ab08591067dd1927b6032a58e800b54c42
Evan R <evanreed0409@gmail.com>
2023-06-15 01:43:22 -0400

    added passgen.h

---
+++ passgen.h
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "string.h"
+
+int getLength();
+void generateString(int length, char string[]);
+void printString(char string[]);
