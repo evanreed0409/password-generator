eaa1ebcc984ff5b9ece6fd2789f07a87d784e5bf
Evan R <evanreed0409@gmail.com>
2023-06-15 16:34:38 -0400

    updated function declarations

--- passgen.h
+++ passgen.h
@@ -1,8 +1,8 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-int getLength();
+void getLength(int * length);
 void generateString(int length, char string[]);
 void printString(char string[]);
