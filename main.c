7f28d2c9b394ab9b74a64d69998c4ff17a5d1d0d
Evan R <evanreed0409@gmail.com>
2023-06-15 16:33:28 -0400

    updated driver code

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
-#include "passgen.c"
 #include "passgen.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 int main(void)
 {
   int length;
 
-  length = getLength;
+  getLength(&length);
 
   char * string = malloc(sizeof(char) * length);
 
-  if(string == NULL);
+  if(string == NULL)
   {
     printf("malloc fail");
     return(0);
